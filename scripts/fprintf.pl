ba240c37a17ed7b2bcfd16e712faed95e10b0846
jjatria <jjatria@gmail.com>
2016-11-15 17:29:29 +0000

    Perl scripts should replace all newlines and tabs

--- scripts/fprintf.pl
+++ scripts/fprintf.pl
@@ -4,8 +4,8 @@ my $file = shift @ARGV;
 open(my $fh, ">>", $file)
   or die "Can't open < $file: $!";
 
-$ARGV[0] =~ s/\\t/\t/;
-$ARGV[0] =~ s/\\n/\n/;
+$ARGV[0] =~ s/\\t/\t/g;
+$ARGV[0] =~ s/\\n/\n/g;
 
 my $v = sprintf shift @ARGV, @ARGV;
 $v =~ s/\0//;
--- scripts/printf.pl
+++ scripts/printf.pl
@@ -1,7 +1,7 @@
 #!/usr/bin/env perl
 
-$ARGV[0] =~ s/\\t/\t/;
-$ARGV[0] =~ s/\\n/\n/;
+$ARGV[0] =~ s/\\t/\t/g;
+$ARGV[0] =~ s/\\n/\n/g;
 
 my $v = sprintf shift @ARGV, @ARGV;
 $v =~ s/\0//;
