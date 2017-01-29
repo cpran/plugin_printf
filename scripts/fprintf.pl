784d7bea31df66680a7775c73d367270bbbd1fe3
José Joaquín Atria <jjatria@gmail.com>
2017-01-29 15:39:38 +0000

    Perl scripts decode arguments as unicode strings, fixes #1

--- scripts/fprintf.pl
+++ scripts/fprintf.pl
@@ -1,5 +1,7 @@
 #!/usr/bin/env perl
 
+require Encode;
+
 my $file = shift @ARGV;
 open(my $fh, ">>", $file)
   or die "Can't open < $file: $!";
@@ -7,6 +9,6 @@ open(my $fh, ">>", $file)
 $ARGV[0] =~ s/\\t/\t/g;
 $ARGV[0] =~ s/\\n/\n/g;
 
-my $v = sprintf shift @ARGV, @ARGV;
+my $v = sprintf shift @ARGV, map { Encode::decode_utf8($_) } @ARGV;
 $v =~ s/\0//;
 print $fh $v;
--- scripts/printf.pl
+++ scripts/printf.pl
@@ -1,8 +1,11 @@
 #!/usr/bin/env perl
 
+require Encode;
+
 $ARGV[0] =~ s/\\t/\t/g;
 $ARGV[0] =~ s/\\n/\n/g;
 
-my $v = sprintf shift @ARGV, @ARGV;
+my $v = sprintf shift @ARGV, map { Encode::decode_utf8($_) } @ARGV;
+
 $v =~ s/\0//;
 print $v;
